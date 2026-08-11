ced6d3dce24cef4a9738af9cf4c2c6044b876109
monidresses <monidress.garments@gmail.com>
2026-08-11 21:42:02 +0530

    Add Firebase project configuration

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase web configuration for Moni Dresses.
+// This config identifies the Firebase project; Firebase API keys are not treated as secrets.
+export const firebaseConfig = {
+  apiKey: "AIzaSyBZdG23Io-oMElZ5XVmhVLd87-sq136dhY",
+  authDomain: "moni-dresses-db.firebaseapp.com",
+  projectId: "moni-dresses-db",
+  storageBucket: "moni-dresses-db.firebasestorage.app",
+  messagingSenderId: "24076547918",
+  appId: "1:24076547918:web:6cae50157f9c6749ff501f"
+};
