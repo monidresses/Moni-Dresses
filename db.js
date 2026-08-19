f219aa7702e7fcdf481846f80199451ae930e46c
monidresses <monidress.garments@gmail.com>
2026-08-19 13:05:48 +0530

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-// db.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
-import { firebaseConfig } from "./firebase-config.js";
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { app, db };
